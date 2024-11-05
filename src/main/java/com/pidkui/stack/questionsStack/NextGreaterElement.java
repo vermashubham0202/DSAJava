4dd0d877577b0dd69050cea7f65be311bdc96542
Shubham Verma <vermashubham0202@gmail.com>
2024-11-05 15:50:47 +0530

    Next greater element from right in array problem algorithm.

---
+++ src/main/java/com/pidkui/stack/questionsStack/NextGreaterElement.java
@@ -0,0 +1,34 @@
+package com.pidkui.stack.questionsStack;
+
+import java.util.Stack;
+
+public class NextGreaterElement {
+
+    private static int[] nextGreaterElement(int[] arr) {
+        int[] result = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (!stack.isEmpty()) {
+                while (!stack.isEmpty() && (stack.peek() <= arr[i])) {
+                    stack.pop();
+                }
+            }
+            if (stack.isEmpty()) {
+                result[i] = -1;
+            } else {
+                result[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {4, 7, 3, 4, 8, 1};
+        int[] output = nextGreaterElement(input);
+
+        for (int i : output) {
+            System.out.print(i + " ");
+        }
+    }
+}
