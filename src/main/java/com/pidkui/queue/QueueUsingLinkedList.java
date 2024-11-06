1cea7f3621f6eec17a8c63c0509db29fc4b9694e
Shubham Verma <vermashubham0202@gmail.com>
2024-11-06 16:27:13 +0530

    Queue enqueue and dequeue operations implemented.

--- src/main/java/com/pidkui/queue/QueueUsingLinkedList.java
+++ src/main/java/com/pidkui/queue/QueueUsingLinkedList.java
@@ -21,10 +21,56 @@ public class QueueUsingLinkedList {
     }
 
     private void enqueue(int data) {
+        Node newNode = new Node(data);
+        if (isEmpty()) {
+            front = newNode;
+        } else {
+            rear.next = newNode;
+        }
+        rear = newNode;
+        length++;
     }
 
-    private Node dequeue() {
-        return null;
+    private int dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("Queue is empty!");
+        }
+
+        int dequeuedValue = front.data;
+        front = front.next;
+
+        if (front == null) {
+            rear = null;
+        }
+        length--;
+        return dequeuedValue;
+    }
+
+    private void display() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty!");
+        } else {
+            Node tempNode = front;
+            while (tempNode != null) {
+                System.out.print(tempNode.data + " --> ");
+                tempNode = tempNode.next;
+            }
+            System.out.println("null");
+        }
+    }
+
+    private int first() {
+        if (isEmpty()) {
+            throw new RuntimeException("Queue is empty!");
+        }
+        return front.data;
+    }
+
+    private int last() {
+        if (isEmpty()) {
+            throw new RuntimeException("Queue is empty!");
+        }
+        return rear.data;
     }
 
     private int getLength() {
@@ -36,6 +82,20 @@ public class QueueUsingLinkedList {
     }
 
     public static void main(String[] args) {
+        QueueUsingLinkedList queue = new QueueUsingLinkedList();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+        queue.display();
 
+        System.out.println("First Node: " + queue.first());
+        System.out.println("Last Node: " + queue.last());
+
+        System.out.println("Deleted Node: " + queue.dequeue());
+
+        System.out.println("--- After Operation ---");
+        queue.display();
     }
 }
